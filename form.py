abe14c3663b742d33320029f255d63beee0203c7
cecylia <cecylia.borek@gmail.com>
2019-12-03 00:00:43 +0100

    new stuff

--- form.py
+++ form.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from object_class import SearchedObject, ListOfSearched
 
 app = Flask(__name__)
 products = [[{'name': 'telefon', 'price': '20', 'link': 'https://www.google.com'}, {'name': 'telefon1', 'price': '201', 'link': 'https://www.google.com'}, {'name': 'telefon', 'price': '20', 'link': 'https://www.google.com'}, {'name': 'telefon', 'price': '20', 'link': 'https://www.google.com'}, {'name': 'telefon', 'price': '20', 'link': 'https://www.google.com'}], [{'name': 'telefon2', 'price': '202', 'link': 'https://www.google.com'}, {'name': 'telefon3', 'price': '203', 'link': 'https://www.google.com'}]]
@@ -10,8 +11,32 @@ def display_form():
 @app.route('/results')
 def get_results():
     query = request.args
-    print(query)
 
+    searched_products = []
+
+    product1 = SearchedObject(query["product1"], query["price-max1"], query["price-min1"], 
+        query["amount1"])
+    searched_products.append(product1)
+    if query["product2"] is not None:
+        product2 = SearchedObject(query["product2"], query["price-max2"], query["price-min2"], 
+            query["amount2"])
+        searched_products.append(product2)
+    if query["product3"] is not None:
+        product3 = SearchedObject(query["product3"], query["price-max3"], query["price-min3"], 
+            query["amount3"])
+        searched_products.append(product3)
+    if query["product4"] is not None:
+        product4 = SearchedObject(query["product4"], query["price-max4"], query["price-min4"], 
+            query["amount4"])
+        searched_products.append(product4)
+    if query["product5"] is not None:
+        product5 = SearchedObject(query["product5"], query["price-max5"], query["price-min5"], 
+            query["amount5"])
+        searched_products.append(product5)
+
+    searchedObjects = ListOfSearched(searched_products)
+
+    print(searched_products)
     #tutaj wywołuję funkcję Benka i przekazuje jej wynik do html jako results
 
     return render_template('result.html', results = products)
