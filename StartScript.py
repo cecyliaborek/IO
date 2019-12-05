e3380485c88fd8de1b4b2d10a6bcefe950f63eda
Beniamin <beniaminpas@gmail.com>
2019-12-05 22:04:39 +0100

    zmiany

--- StartScript.py
+++ StartScript.py
@@ -6,22 +6,21 @@ def StartScript(List_of_searched: object_class.ListOfSearched):
     file = open("log.txt", "a")
     file.write("Czas rozpoczęcia wykonywania:" + time.ctime(time.time()) + "\n")
     file.close()
+
     time_start = time.time()
     Lof = scraplist(List_of_searched.ListOS)
     file = open("log.txt", "a")
+
     time_scrap = time.time()
     file.write("Czas całkowity scrapowania: " + str(round(time_scrap - time_start, 2)) + "s\n")
+
     FoundSets = object_class.FoundSets()
     Lof.sort_list()
     time_sort = time.time()
     file.write("Czas sortowania: " + str(round(time_sort - time_start, 2)) + "s\n")
-    for i in Lof.lista:
-        if len(i.lista) < 3:
-            limit = len(i.lista)
-        else:
-            limit = 3
 
-        for k in range(0, limit):
+    for i in Lof.lista:
+        for k in range(0, 3):
             if len(i.lista) > 0:
                 item = i.lista.pop(0)
                 founditem = object_class.FoundItem(item.name, item.price * item.amount + item.price_ship, item.rate,
@@ -30,10 +29,11 @@ def StartScript(List_of_searched: object_class.ListOfSearched):
                 founditem = object_class.FoundItem('', 0, 0, '', False)
             FoundSets.add_to_list(k, founditem, founditem.price)
 
-    for lis in FoundSets.list:  # removal of unfilled lists
-        for item in lis:
-            if not item.is_found:
-                FoundSets.list.remove(lis)
+    # for lis in FoundSets.list:  # removal of unfilled lists
+    #     for item in lis:
+    #         if not item.is_found:
+    #             FoundSets.list.remove(lis)
+
     time_sets = time.time()
     file.write("Czas tworzenia zestawów i ich sprawdzania: " + str(round(time_sets - time_start, 2)) + "s\n")
     file.write("Czas całkowity wykonywania funkcji StartScript: " + str(round(time.time() - time_start, 2)) + "s\n")
@@ -45,11 +45,10 @@ def scraplist(list_of_searched):
     timeperscrap = []
     f = open("log.txt", "a")
     foundlist = object_class.List(len(list_of_searched))
+    timeperscrap.append(time.time())
     for item in list_of_searched:
-        timeperscrap.append(time.time())
         scrap = scraper.Scraper(item.name)
         scrap.run()
-        timeperscrap.append(time.time())
         foundobjlist = object_class.Obj_list()
         for found in scrap.products_list:
             if not found["deliver_cost"] == None:
@@ -57,7 +56,7 @@ def scraplist(list_of_searched):
                                         int(found["rate"]),
                                         int(found["rate_number"]), int(item.amount), found["url"])
         foundlist.set_objlist(list_of_searched.index(item), foundobjlist)
-
+        timeperscrap.append(time.time())
     for timestamp in timeperscrap:
         if not timeperscrap.index(timestamp) == 0:
             f.write("czas wykonanie scrapu przedmiotu" + str(timeperscrap.index(timestamp)) + " wynosi: " + str(
